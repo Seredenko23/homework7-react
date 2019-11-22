390beaf76d8cdeac13cbb2ee7c525b8b6a288e77
unknown <srvladyk555@gmail.com>
2019-11-22 14:58:06 +0200

    burger work correctly

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import './App.css'
 function App () {
   return (
     <div className='App'>
-      <Burger />
+      <Burger>
+        <p>Lorem Ipsum 1</p>
+        <p>Lorem Ipsum 2</p>
+        <p>Lorem Ipsum 3</p>
+        <p>Lorem Ipsum 4</p>
+        <p>Lorem Ipsum 5</p>
+      </Burger>
       <Voter />
     </div>
   )
--- src/components/burger/burger.js
+++ src/components/burger/burger.js
@@ -5,14 +5,33 @@ class Burger extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      opened: false
+      opened: true
     }
+
+    this.menuRef = React.createRef()
+
+    this.show = () => {
+    	if (this.state.opened) {
+    		this.menuRef.current.style.transform = 'translateX(-300px)'
+    	} else {
+    		this.menuRef.current.style.transform = 'translateX(0px)'
+    	}
+  		this.setState({
+  			opened: !this.state.opened
+  		})
+  	}
   }
 
   render () {
     return (
-      <div className='menu'>
-        <div className='cross' />
+      <div className='menu-wrapper'>
+        <div className='burger' onClick={this.show} />
+        <div className='menu' ref={this.menuRef}>
+          <div className='burger' onClick={this.show} />
+          	<div className='contentWrapper'>
+            {this.props.children}
+          	</div>
+        </div>
       </div>
     )
   }
